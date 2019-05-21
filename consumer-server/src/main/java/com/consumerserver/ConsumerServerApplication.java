e23d58731cc28563311439c40a4c87b1d2e632a9
zhangzhanming <zhanming-zhang@human-horizons.com>
2019-05-21 15:55:21 +0800

    代码更新

---
+++ consumer-server/src/main/java/com/consumerserver/ConsumerServerApplication.java
@@ -0,0 +1,16 @@
+package com.consumerserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ConsumerServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumerServerApplication.class, args);
+    }
+
+}
